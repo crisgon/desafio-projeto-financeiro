5800a116340df88dbdce69fc1c8dee6543cbf171
IsabellaSoares <isabella.soares.28@gmail.com>
2024-10-06 16:19:47 -0300

    test: add stories and tests

--- packages/ui/src/button/button.stories.tsx
+++ packages/ui/src/button/button.stories.tsx
@@ -3,41 +3,29 @@ import { fn } from "@storybook/test";
 
 import { Button } from ".";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Components/Button",
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: "centered",
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
 } as Meta;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof Button>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Contained: Story = {
   args: {
-    children: "Contained",
+    label: "Contained",
     variant: "contained",
   },
 };
 
 export const Outlined: Story = {
   args: {
-    children: "Outlined",
+    label: "Outlined",
     variant: "outlined",
-  },
-};
-
-export const Text: Story = {
-  args: {
-    children: "Text",
-    variant: "text",
   },
 };
