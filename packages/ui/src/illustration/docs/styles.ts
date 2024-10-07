02cede5a4ef03bf3c073e8da30f538bbaf11fa74
IsabellaSoares <isabella.soares.28@gmail.com>
2024-10-06 21:24:06 -0300

    docs: add component stories

---
+++ packages/ui/src/illustration/docs/styles.ts
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  li {
+    margin-bottom: 60px;
+    gap: 24px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  span {
+    font-weight: bold;
+    font-size: 18px;
+  }
+`;
---
+++ packages/ui/src/illustration/illustration.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Illustration as IllustrationComponent } from ".";
+import { ListAllIllustrationsDocs } from "./docs/ListAllIllustrationDocs";
+
+const meta = {
+  title: "Components/Illustration",
+  component: IllustrationComponent,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    name: {
+      description: "Define a ilustração a ser apresentada",
+      control: "select",
+    },
+  },
+} as Meta;
+
+export default meta;
+type Story = StoryObj<typeof IllustrationComponent>;
+
+export const Illustration: Story = {
+  args: {
+    name: "error404",
+  },
+};
+
+export const ListAll = () => <ListAllIllustrationsDocs />;
