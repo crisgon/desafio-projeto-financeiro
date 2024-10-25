071704744029fa72a37fd1f44f5073202d0143be
IsabellaSoares <isabella.soares.28@gmail.com>
2024-10-25 12:05:23 -0300

    feat: landing page desktop layout

--- apps/landing_page/app/page.tsx
+++ apps/landing_page/app/page.tsx
@@ -1,95 +1,143 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { Box, Typography, useTheme } from "@mui/material";
+import { Footer } from "@repo/ui/footer";
+import { Illustration } from "@repo/ui/illustration";
+import styles from "./page.styles";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const theme = useTheme();
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  return (
+    <>
+      <header className="header">Header</header>
+      <main
+        style={{ maxWidth: "1200px", margin: "0 auto", marginBottom: "72px" }}
+      >
+        <Box
+          display="flex"
+          flexDirection="row"
+          gap="16px"
+          alignItems="center"
+          marginY="40px"
+        >
+          <Typography fontSize="28px" fontWeight="600">
+            Experimente mais liberdade no controle da sua vida financeira.
+            <br />
+            Crie sua conta com a gente!
+          </Typography>
+          <Box>
+            <Illustration name="report" />
+          </Box>
+        </Box>
+        <Box
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
+          gap="40px"
+        >
+          <Typography variant="h1">Vantagens do nosso banco:</Typography>
+          <Box
+            display="flex"
+            alignItems="center"
+            flexDirection="row"
+            gap={theme.spacing("large")}
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+            <Box
+              display="flex"
+              alignItems="center"
+              flexDirection="column"
+              width="279px"
+              gap={theme.spacing("medium")}
+            >
+              <Illustration name="gift" />
+              <Typography
+                color={theme.palette.primary.main}
+                variant="subtitle1"
+              >
+                Conta e cartão gratuitos
+              </Typography>
+              <Typography
+                color={theme.palette.grey[500]}
+                variant="body1"
+                textAlign="center"
+              >
+                Isso mesmo, nossa conta é digital, sem custo fixo e mais que
+                isso: sem tarifa de manutenção.
+              </Typography>
+            </Box>
+            <Box
+              display="flex"
+              alignItems="center"
+              flexDirection="column"
+              width="279px"
+              gap={theme.spacing("medium")}
+            >
+              <Illustration name="draw" />
+              <Typography
+                color={theme.palette.primary.main}
+                variant="subtitle1"
+              >
+                Saques sem custo
+              </Typography>
+              <Typography
+                color={theme.palette.grey[500]}
+                variant="body1"
+                textAlign="center"
+              >
+                Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.
+              </Typography>
+            </Box>
+            <Box
+              display="flex"
+              alignItems="center"
+              flexDirection="column"
+              width="279px"
+              gap={theme.spacing("medium")}
+            >
+              <Illustration name="points" />
+              <Typography
+                color={theme.palette.primary.main}
+                variant="subtitle1"
+              >
+                Programa de pontos
+              </Typography>
+              <Typography
+                color={theme.palette.grey[500]}
+                variant="body1"
+                textAlign="center"
+              >
+                Você pode acumular pontos com suas compras no crédito sem pagar
+                mensalidade!
+              </Typography>
+            </Box>
+            <Box
+              display="flex"
+              alignItems="center"
+              flexDirection="column"
+              width="279px"
+              gap={theme.spacing("medium")}
+            >
+              <Illustration name="devices" />
+              <Typography
+                color={theme.palette.primary.main}
+                variant="subtitle1"
+              >
+                Seguro Dispositivos
+              </Typography>
+              <Typography
+                color={theme.palette.grey[500]}
+                variant="body1"
+                textAlign="center"
+              >
+                Seus dispositivos móveis (computador e laptop) protegidos por
+                uma mensalidade simbólica.
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file-text.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <Footer />
+    </>
   );
 }
