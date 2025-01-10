5b7fa4303a68e39982db0c136bdd5bb9e2f68536
IsabellaSoares <isabella.soares.28@gmail.com>
2025-01-09 22:58:39 -0300

    refactor: delete unused local api endpoints

--- apps/dashboard/app/recoil/atoms/extractAtom.ts
+++
@@ -1,9 +0,0 @@
-import type { GroupedTransaction } from "app/types/transaction";
-import { atom } from "recoil";
-
-const extract: GroupedTransaction[] = [];
-
-export const extractState = atom({
-  key: "extractState",
-  default: extract,
-});
